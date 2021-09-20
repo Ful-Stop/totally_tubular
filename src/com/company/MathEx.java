83567ff02241b526df538c9f89b2bee5eddb1395
Patrick <ptrinkz@gmail.com>
2021-09-20 12:04:14 -0400

    message

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -2,12 +2,29 @@ package com.company;
 
 public class MathEx {
     public static int digitSwapper(int input) {
-        int ones = (input % 100) % 10;
-        int inputExtra = ones;
-        ones *= 10;
+        int ones = input % 10;
         int tens = (input % 100) - ones;
-        input -= tens
+        int inputExtra = input - ones - tens;
+        ones *= 10;
         tens /= 10;
-        System.out.println(input);
+        int finalValue = inputExtra + tens + ones;
+        System.out.println(finalValue);
+        return finalValue;
+    }
+    public static int timeLeft(int curHour, int curMin, int depHour, int depMin) {
+        int curTotal = curMin + (curHour * 60);
+        int depTotal = depMin + (depHour * 60);
+        int totalHour = (depTotal - curTotal) / 60;
+        int totalMin = (depTotal - curTotal) % 60;
+        System.out.println(totalHour + " hours " + totalMin + " minute(s)");
+        return totalHour + totalMin;
+    }
+    public static int dayOfWeek(int DOTW, int date) {
+
+    }
+
+    public static void main(String[] args) {
+        digitSwapper(143);
+        timeLeft(2, 15, 6, 10);
     }
 }
